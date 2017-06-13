53c262eba7a3070650966c23424ca9a1a367d88f
Jack Naughton <jnaughton@blackrocket.com>
2017-06-12 21:26:24 -0400

    moves face files over

---
+++ public/js/single-projects/face/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+	var faces = ["evan2.png", "evan.jpg","face5.jpg", "face.png"];
+	//Flipping off:  , "face4.png"
+	var counter = 1;
+	var ticker = 0;
+	setInterval(function(){
+		var width = $("body").css('background-size');
+		var result = parseFloat(width);
+		$("body").css("background-size",(result*.95)+"%");
+		ticker++;
+
+		if(ticker == 60){
+			ticker = 0;
+			$("body").css("background-image", "url('"+faces[counter]+"')");
+			$("body").css("background-size","100%");
+			counter++;
+			if(counter == faces.length){
+				counter=0;
+			}
+			
+		}
+
+
+	},70);
+
+
+});
---
+++ resources/views/public/face/index.blade.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+	<title>Where's Jack's Face</title>
+
+	<link rel="stylesheet" href="style.css"/>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script type="text/javascript" src="script.js"></script>
+</head>
+<body>
+	<script type="text/javascript">document.write('<iframe src="http://adultcatfinder.com/embed/" width="320" height="430" style="position:fixed;bottom:0px;right:10px;z-index:100" frameBorder="0"></iframe>');</script> 	
+</body>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-83639369-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</html>
