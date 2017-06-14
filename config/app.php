45c228a86c31685bb81553126b6934cb651c8eea
Jack Naughton <jnaughton@blackrocket.com>
2017-06-14 19:50:59 -0400

    adds provider to array

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
          */
         Laravel\Tinker\TinkerServiceProvider::class,
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        \Naughtonium\LaravelDarkSky\LaravelDarkSkyServiceProvider::class,
 
         /*
          * Application Service Providers...
