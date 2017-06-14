55ef7298cfc6cf9c3e8c21f3db9318a643d34cf4
Jack Naughton <jnaughton@blackrocket.com>
2017-06-14 19:34:19 -0400

    adds csrf field

--- resources/views/public/jp/index.blade.php
+++ resources/views/public/jp/index.blade.php
@@ -2,6 +2,7 @@
 <title>Hi</title>
 <body style="text-align: center;">
 <form method="POST" action="jp" style="position: relative; top: 30%;">
+    {{ csrf_field() }}
     <input name="input" autofocus style="width: 80%; font-size: 10em;" autocapitalize="off" autocorrect="off">
 </form>
 </body>
