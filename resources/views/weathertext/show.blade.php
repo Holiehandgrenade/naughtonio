f0e8601aea3821e05415bf308194f069bcd069f8
Jack Naughton <jnaughton@blackrocket.com>
2017-06-15 15:08:58 -0400

    adds form elements

--- resources/views/weathertext/show.blade.php
+++ resources/views/weathertext/show.blade.php
@@ -1,33 +1,62 @@
 @extends('layouts.app')
 
 @section('content')
-    {{--{{ Form::open([--}}
-        {{--'url' => 'weather-text',--}}
-        {{--'method' => 'put',--}}
-        {{--'class' => 'form-horizontal',--}}
-    {{--]) }}--}}
+    {{ Form::open([
+        'url' => 'weather-text',
+        'method' => 'put',
+        'class' => 'form-horizontal',
+    ]) }}
 
-    {{--{{ Form::text('phone', $user->phone) }}--}}
+        <div class="form-group{{ $errors->has('phone') ? ' has-error' : '' }}">
+            {!! Form::label('phone', null, ['class' => 'col-md-4 control-label'], false) !!}
 
-    {{--{{ Form::close() }}--}}
+            <div class="col-md-6">
+                {!! Form::text('phone', $user->phone, ['class' => 'form-control']) !!}
+                @if ($errors->has('phone'))
+                    <span class="help-block">
+                    <strong>{{ $errors->first('phone') }}</strong>
+                </span>
+                @endif
+            </div>
+        </div>
 
+        <div class="form-group{{ $errors->has('time') ? ' has-error' : '' }}">
+            {!! Form::label('time', null, ['class' => 'col-md-4 control-label'], false) !!}
 
-    {{--<div class="form-group{{ $errors->has('first_name') ? ' has-error' : '' }}">--}}
-        {{--{!! Form::label('first_name', 'First Name <span class="beanstalk-required"></span>', ['class' => 'col-md-4 control-label'], false) !!}--}}
+            <div class="col-md-6">
+                {!! Form::text('time', $weatherText->time, ['class' => 'form-control']) !!}
+                @if ($errors->has('time'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('time') }}</strong>
+                    </span>
+                @endif
+            </div>
+        </div>
 
-        {{--<div class="col-md-6">--}}
-            {{--{!! Form::text('first_name', $user->first_name, ['class' => 'form-control']) !!}--}}
-            {{--@if ($errors->has('first_name'))--}}
-                {{--<span class="help-block">--}}
-                {{--<strong>{{ $errors->first('first_name') }}</strong>--}}
-            {{--</span>--}}
-            {{--@endif--}}
-        {{--</div>--}}
-    {{--</div>--}}
+        <div class="form-group{{ $errors->has('timezone') ? ' has-error' : '' }}">
+            {!! Form::label('timezone', null, ['class' => 'col-md-4 control-label'], false) !!}
 
+            <div class="col-md-6">
+                {!! Form::text('timezone', $user->timezone, ['class' => 'form-control']) !!}
+                @if ($errors->has('timezone'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('timezone') }}</strong>
+                    </span>
+                @endif
+            </div>
+        </div>
 
-    <div>Phone </div>
-    <div>Time </div>
-    <div>Timezone </div>
-    <div>Active </div>
+        <div class="form-group{{ $errors->has('active') ? ' has-error' : '' }}">
+            {!! Form::label('active', null, ['class' => 'col-md-4 control-label'], false) !!}
+
+            <div class="col-md-6">
+                {!! Form::text('active', $weatherText->active, ['class' => 'form-control']) !!}
+                @if ($errors->has('active'))
+                    <span class="help-block">
+                            <strong>{{ $errors->first('active') }}</strong>
+                        </span>
+                @endif
+            </div>
+        </div>
+    {{ Form::close() }}
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Auth::login(\App\User::first());
 Route::get('/', function () {
     return view('welcome');
 });
