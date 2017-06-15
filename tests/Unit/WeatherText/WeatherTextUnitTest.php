62b28bae49cabfc26018c12560bc2c81fcf1c192
Jack Naughton <jnaughton@blackrocket.com>
2017-06-15 10:29:59 -0400

    adds first test

--- tests/Unit/WeatherText/WeatherTextUnitTest.php
+++ tests/Unit/WeatherText/WeatherTextUnitTest.php
@@ -2,11 +2,19 @@
 
 namespace Tests\Unit\WeatherText;
 
+use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class WeatherTextUnitTest extends TestCase
 {
-    
+    use DatabaseTransactions;
+
+    /** @test */
+    public function a_user_has_nullable_phone()
+    {
+        $user = factory(User::class)->create(['phone' => null]);
+        $this->assertDatabaseHas('users', $user);
+    }
 }
