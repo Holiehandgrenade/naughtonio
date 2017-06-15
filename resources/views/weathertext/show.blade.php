a041151bc7dd325c0e4fc06486736adf47d558ab
Jack Naughton <jnaughton@blackrocket.com>
2017-06-15 13:43:43 -0400

    meh

--- resources/views/weathertext/show.blade.php
+++ resources/views/weathertext/show.blade.php
@@ -1,14 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-    {{ Form::open([
-        'url' => 'weather-text',
-        'method' => 'put'
-    ]) }}
+    {{--{{ Form::open([--}}
+        {{--'url' => 'weather-text',--}}
+        {{--'method' => 'put',--}}
+        {{--'class' => 'form-horizontal',--}}
+    {{--]) }}--}}
 
-    {{ Form::text('phone', $user->phone) }}
+    {{--{{ Form::text('phone', $user->phone) }}--}}
 
-    {{ Form::close() }}
+    {{--{{ Form::close() }}--}}
+
+
+    {{--<div class="form-group{{ $errors->has('first_name') ? ' has-error' : '' }}">--}}
+        {{--{!! Form::label('first_name', 'First Name <span class="beanstalk-required"></span>', ['class' => 'col-md-4 control-label'], false) !!}--}}
+
+        {{--<div class="col-md-6">--}}
+            {{--{!! Form::text('first_name', $user->first_name, ['class' => 'form-control']) !!}--}}
+            {{--@if ($errors->has('first_name'))--}}
+                {{--<span class="help-block">--}}
+                {{--<strong>{{ $errors->first('first_name') }}</strong>--}}
+            {{--</span>--}}
+            {{--@endif--}}
+        {{--</div>--}}
+    {{--</div>--}}
 
 
     <div>Phone </div>
