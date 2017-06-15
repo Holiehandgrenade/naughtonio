7f43480f6dfbf938e6da32a064cf6508252770a6
Jack Naughton <jnaughton@blackrocket.com>
2017-06-15 11:42:29 -0400

    adds test if user has phone

--- tests/Browser/WeatherText/WeatherTextBrowserTest.php
+++ tests/Browser/WeatherText/WeatherTextBrowserTest.php
@@ -20,4 +20,18 @@ class WeatherTextBrowserTest extends DuskTestCase
                     ->assertSee('Phone:');
         });
     }
+
+    /** @test */
+    public function if_user_has_no_phone_they_should_see_a_time_timezone_active_input()
+    {
+        $user = factory(User::class)->create(['phone' => null]);
+
+        $this->browse(function ($browser) use ($user) {
+            $browser->loginAs($user)
+                ->visit('/weather-text')
+                ->assertSee('Time:')
+                ->assertSee('Timezone:')
+                ->assertSee('Active:');
+        });
+    }
 }
