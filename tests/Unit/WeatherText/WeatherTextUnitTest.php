c209446d6d63f0014bd74360fc5f61287d447ca7
Jack Naughton <jnaughton@blackrocket.com>
2017-06-15 10:28:33 -0400

    creates weathertext test

---
+++ tests/Unit/WeatherText/WeatherTextUnitTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Unit\WeatherText;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class WeatherTextUnitTest extends TestCase
+{
+    
+}
