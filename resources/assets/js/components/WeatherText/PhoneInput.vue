4604cd1b25dec74c22304326e3d857d46c8b2aaa
Jack Naughton <jnaughton@blackrocket.com>
2017-06-15 12:33:30 -0400

    getting phone input working

--- resources/assets/js/components/WeatherText/PhoneInput.vue
+++ resources/assets/js/components/WeatherText/PhoneInput.vue
@@ -2,22 +2,34 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Example Component</div>
-
-                    <div class="panel-body">
-                        I'm an example component!
-                    </div>
-                </div>
+                <masked-input
+                    name="phone"
+                    mask="(111) 111-1111"
+                    placeholder="Please input"
+                    @input="phone = arguments[1]"
+                />
+                <button @click="log(phone)"></button>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import MaskedInput from 'vue-masked-input';
+
     export default {
-        mounted() {
-            console.log('Component mounted.')
+        data () {
+            return {
+                phone: '',
+            }
+        },
+        components: {
+          MaskedInput
+        },
+        methods: {
+            log (v) {
+                console.log(v);
+            }
         }
     }
 </script>
