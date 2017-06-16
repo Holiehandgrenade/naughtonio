eb196013b088f387a097f05efa8fa6056fca7990
Jack Naughton <jnaughton@blackrocket.com>
2017-06-16 14:55:44 -0400

    adds a test to make sure within fifteen minutes works

--- tests/Unit/WeatherText/WeatherTextUnitTest.php
+++ tests/Unit/WeatherText/WeatherTextUnitTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Unit\WeatherText;
 
+use App\Models\WeatherText\WeatherText;
 use App\User;
+use Carbon\Carbon;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
@@ -16,5 +18,27 @@ class WeatherTextUnitTest extends TestCase
     {
         $user = factory(User::class)->create(['phone' => null]);
         $this->assertDatabaseHas('users', $user['attributes']);
+    }
+
+    /** @test */
+    public function can_get_all_weather_texts_in_a_fifteen_minute_block()
+    {
+        $userOne = factory(User::class)->create();
+        $userTwo = factory(User::class)->create();
+        $weatherTextOne = new WeatherText([
+            'time' => Carbon::now()->addMinutes(5)->format('H:i'),
+            'active' => 1,
+        ]);
+        $weatherTextTwo = new WeatherText([
+            'time' => Carbon::now()->addMinutes(20)->format('H:i'),
+            'active' => 1,
+        ]);
+        $userOne->weatherText()->save($weatherTextOne);
+        $userTwo->weatherText()->save($weatherTextTwo);
+
+        $fiftenMinutes = WeatherText::withinFifteenMinutes()->get();
+
+        $this->assertTrue($fiftenMinutes->contains($weatherTextOne));
+        $this->assertFalse($fiftenMinutes->contains($weatherTextTwo));
     }
 }
