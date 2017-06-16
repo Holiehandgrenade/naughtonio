b45f0d69a9153484462f1bd4cb1b1000b47bae94
Jack Naughton <jnaughton@blackrocket.com>
2017-06-16 13:11:23 -0400

    adds key value pairs for time and timezone arrays

--- app/Models/WeatherText/WeatherText.php
+++ app/Models/WeatherText/WeatherText.php
@@ -2,9 +2,15 @@
 
 namespace App\Models\WeatherText;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class WeatherText extends Model
 {
     protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
