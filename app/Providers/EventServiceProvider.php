add91c2d85af7b61b3612332954e068bd2cbbcd3
Jack Naughton <jnaughton@blackrocket.com>
2017-06-16 12:47:01 -0400

    removes from event service provider

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Events\WeatherTextUpdated;
-use App\Listeners\UpdateUserAndWeatherText;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
@@ -15,9 +13,7 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        WeatherTextUpdated::class => [
-            UpdateUserAndWeatherText::class,
-        ],
+
     ];
 
     /**
