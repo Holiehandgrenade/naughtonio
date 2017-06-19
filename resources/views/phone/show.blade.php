ae3be136ba6d88a796a1ea37d8a7bf7a32cc83ff
Jack Naughton <jnaughton@blackrocket.com>
2017-06-19 14:44:07 -0400

    phone enter simple blade form

--- resources/views/phone/show.blade.php
+++ resources/views/phone/show.blade.php
@@ -1,5 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-    <weathertext-phone-input></weathertext-phone-input>
+    {{ Form::open([
+        'url' => 'phone',
+        'method' => 'post',
+        'class' => 'form-horizontal',
+    ]) }}
+
+        <div class="form-group{{ $errors->has('phone') ? ' has-error' : '' }}">
+            {!! Form::label('phone', null, ['class' => 'col-md-4 control-label'], false) !!}
+
+            <div class="col-md-6">
+                {!! Form::text('phone', isset($phone) ? $phone : null, ['class' => 'form-control']) !!}
+                @if ($errors->has('phone'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('phone') }}</strong>
+                    </span>
+                @endif
+            </div>
+        </div>
+
+        <div class="form-group{{ $errors->has('active') ? ' has-error' : '' }}">
+            <div class="col-md-6">
+                {!! Form::submit('Save', ['class' => 'form-control']) !!}
+            </div>
+        </div>
+
+    {{ Form::close() }}
+
 @endsection
