e16b1c66e1cec23be405b25836d55d2f4cd9473d
Jack Naughton <jnaughton@blackrocket.com>
2017-06-19 13:18:09 -0400

    creates phone verifications table

---
+++ database/migrations/2017_06_19_171428_phone_verifications.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class PhoneVerifications extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('phone_verifications', function (Blueprint $table) {
+            $table->mediumIncrements('id');
+            $table->integer('user_id');
+            $table->string('pending_phone');
+            $table->string('verify_code');
+            $table->unsignedInteger('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('phone_verifications');
+    }
+}
