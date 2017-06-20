381c35b973f425d596b599c19be926ef630445df
Jack Naughton <jnaughton@blackrocket.com>
2017-06-20 12:06:40 -0400

    writing phone controller feature tests

--- config/app.php
+++ config/app.php
@@ -207,6 +207,7 @@ return [
         'Config' => Illuminate\Support\Facades\Config::class,
         'Cookie' => Illuminate\Support\Facades\Cookie::class,
         'Crypt' => Illuminate\Support\Facades\Crypt::class,
+        'DarkSky' => \Naughtonium\LaravelDarkSky\Facades\DarkSky::class,
         'DB' => Illuminate\Support\Facades\DB::class,
         'Eloquent' => Illuminate\Database\Eloquent\Model::class,
         'Event' => Illuminate\Support\Facades\Event::class,
--- tests/Feature/PhoneVerification/PhoneVerificationFeatureTest.php
+++ tests/Feature/PhoneVerification/PhoneVerificationFeatureTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\PhoneVerification;
 
+use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -9,13 +10,29 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class PhoneVerificationFeatureTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    /** @test */
+    public function phone_is_required()
     {
-        $this->assertTrue(true);
+        $user = factory(User::class)->create();
+        $this->be($user);
+
+        $this->post('/phone', [])
+            ->assertStatus(302);
+        $this->assertDatabaseMissing('phone_verifications', ['phone' => '5555555555']);
+    }
+
+    /** @test */
+    public function phone_must_be_10_characters()
+    {
+        $user = factory(User::class)->create();
+        $this->be($user);
+
+        $this->post('/phone', ['phone' => '123456789'])
+            ->assertStatus(302);
+        $this->assertDatabaseMissing('phone_verifications', ['phone' => '123456789']);
+
+        $this->post('/phone', ['phone' => '12345678922'])
+            ->assertStatus(302);
+        $this->assertDatabaseMissing('phone_verifications', ['phone' => '12345678922']);
     }
 }
