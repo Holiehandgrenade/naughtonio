c09eb2e35cdabdd80dce15afabbe4103bafaaf7e
Jack Naughton <jnaughton@blackrocket.com>
2017-06-20 14:58:20 -0400

    adds test if process goes well

--- tests/Browser/PhoneVerification/PhoneVerificationBrowserTest.php
+++ tests/Browser/PhoneVerification/PhoneVerificationBrowserTest.php
@@ -23,9 +23,30 @@ class PhoneVerificationBrowserTest extends DuskTestCase
                 ->visit('/phone')
                 ->type('phone', '5555555555')
                 ->click('input[type="submit"]');
-            $browser->assertPathIs('/phone-verify');
+            $browser->assertPathIs('/phone-verify')
+                ->assertSee('A verification code has been sent to this number.');
         });
 
         $this->assertQueued([SendPhoneVerificationText::class]);
+    }
+
+    /** @test */
+    public function if_code_matches_continue_to_intended_url_after_going_through_verify_page()
+    {
+        $user = factory(User::class)->create(['phone' => null]);
+
+        $this->browse(function ($browser) use ($user) {
+            $browser->loginAs($user)
+                ->visit('/weather-text') // has middleware requires-phone
+                ->type('phone', '5555555555')
+                ->click('input[type="submit"]');
+
+            $code = \DB::table('phone_verifications')->whereUserId($user->id)->get()->last()->verify_code;
+
+            $browser->type('code', $code)
+                ->click('input[type="submit"]');
+
+            $browser->assertPathIs('/weather-text');
+        });
     }
 }
