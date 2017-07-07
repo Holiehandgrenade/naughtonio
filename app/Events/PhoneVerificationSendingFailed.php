dc9e2958fddce06d393794a4648852d8b458aef5
Jack Naughton <jnaughton@blackrocket.com>
2017-07-07 09:30:43 -0400

    sets up failed verifcation event

---
+++ app/Events/PhoneVerificationSendingFailed.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class PhoneVerificationSendingFailed implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $verification;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($verification)
+    {
+        $this->verification = $verification;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('phone-verify.' . $this->verification->id);
+    }
+}
