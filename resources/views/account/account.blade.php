7be9007db1fb5a5f57ce9f5697bce775f43198c3
Jack Naughton <jnaughton@blackrocket.com>
2017-07-11 11:50:14 -0400

    dusk coming along, realized there was another feature i needed to do

--- resources/views/account/account.blade.php
+++ resources/views/account/account.blade.php
@@ -26,6 +26,32 @@
                 </div>
             </div>
 
+            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                {!! Form::label('email', null, ['class' => 'col-sm-2 control-label'], false) !!}
+                <div class="col-sm-9">
+                    {!! Form::text('email', $user->email, ['class' => 'form-control']) !!}
+                    @if ($errors->has('email'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                    @endif
+                </div>
+            </div>
+
+
+            <div class="form-group{{ $errors->has('current_password') ? ' has-error' : '' }}">
+                {!! Form::label('current password', null, ['class' => 'col-sm-2 control-label'], false) !!}
+                <div class="col-sm-9">
+                    {!! Form::text('current_password', null, ['class' => 'form-control']) !!}
+                    @if ($errors->has('current_password'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('current_password') }}</strong>
+                        </span>
+                    @endif
+                </div>
+            </div>
+
+
             <div class="form-group{{ $errors->has('active') ? ' has-error' : '' }}">
                 <div class="col-sm-offset-2 col-sm-3">
                     {!! Form::submit('Submit', ['class' => 'form-control btn btn-primary']) !!}
