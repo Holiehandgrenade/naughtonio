68d28c10bd8984f3d3f352df08c8b436fda65e54
Jack Naughton <jnaughton@blackrocket.com>
2017-12-18 14:57:17 -0700

    mirrors app secret code

--- app/Http/Controllers/TwitterController.php
+++ app/Http/Controllers/TwitterController.php
@@ -13,11 +13,21 @@ class TwitterController extends Controller
         $timestamp = $request->input('timestamp');
         $code = $request->input('code');
 
-        \Log::info($timestamp);
+        $split = str_split($timestamp);
+        \Log::info($split);
+        foreach($split as $key => $num) {
+            $split[$key] = ord($num);
+        }
+        \Log::info($split);
+
+        if ($code == implode($split)) {
+            Twitter::postTweet(['status' => $status]);
+
+            return json_encode(["status" => 200, "message" => "sent tweet"]);
+        }
         \Log::info($code);
+        \Log::info(implode($split));
 
-//        Twitter::postTweet(['status' => $status]);
-
-        return json_encode(["status" => 200, "message" => "sent tweet"]);
+        return json_encode(["status" => 400, "message" => "bad code"]);
     }
 }
